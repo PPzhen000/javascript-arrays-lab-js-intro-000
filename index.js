4e0931befd0542616a5c6fd421decfdc1b4a91bf
Ariel Zhen <zhenpingping@live.com>
2018-04-25 05:24:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// const app = "I don't do much."
-//
-// function destructivelyAppendKitten(kittens, kittenname){
-//   kittens.push(kittenname);
-//   return kittens;
-// }
+const app = "I don't do much."
+
+function destructivelyAppendKitten(kittens, kittenname){
+  kittens.push(kittenname);
+  return kittens;
+}
